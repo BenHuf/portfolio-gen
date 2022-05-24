eba381ad902fe0a7e3e60aba928022ea2880f492
BenHuf <benthehuffman@gmail.com>
2022-05-24 13:39:09 -0400

    because it told me to

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-    console.log('Porfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Porfolio complete! Check out index.html to see the output!');
+// });
+
